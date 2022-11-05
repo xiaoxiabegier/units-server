0cb08769cb5a03393c244112f1b55f60d294595a
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-05 11:21:00 -0700

    intial

---
+++ pages/api/index.js
@@ -0,0 +1,69 @@
+import { collection, getDocs , doc, getDoc} from "firebase/firestore";
+import {db} from "../../public/firebase";
+export default async function handler(req, res){
+
+//    if(req.method === "POST") {
+//
+//    }
+//    if(req.method === "PUT") {
+//
+//    }
+    const {includefields, omitfields, propertyid} = req.headers
+
+    let propertyIDArray = []
+    let omitFieldsArray = []
+    let includeFieldsArray = []
+
+    if(typeof propertyid != "undefined")  propertyIDArray = propertyid.split(',')
+    if(typeof omitfields != "undefined") omitFieldsArray = omitfields.split(',')
+    if(typeof includefields != "undefined") includeFieldsArray = includefields.split(',')
+
+    let returnObj = await getAppropriateDocs(propertyIDArray, includeFieldsArray,omitFieldsArray )
+
+    res.status(200).json(returnObj)
+
+}
+
+async function getFields(doc, includeFieldsArray,  omitFieldsArray) {
+
+
+    let returnObj = {}
+
+    if (includeFieldsArray.length !== 0) {
+        // GET SPECIFIED FIELDS
+        for(let elem in includeFieldsArray) {
+            returnObj[includeFieldsArray[elem]] = doc.get(includeFieldsArray[elem])
+        }
+    } else {
+        returnObj = doc.data()
+    }
+
+    for(let elem in omitFieldsArray){
+        delete returnObj[omitFieldsArray[elem]]
+    }
+
+    return returnObj
+}
+
+async function getAppropriateDocs(propertyIDArray, includeFieldsArray, omitFieldsArray){
+    let data = {}
+
+    if(propertyIDArray.length === 0) {
+        // GET ALL DOCS IN /UNITS
+        const querySnapshot = await getDocs(collection(db, "units"));
+        querySnapshot.forEach(async (doc) => {
+            data[doc.id] = await getFields(doc, includeFieldsArray, omitFieldsArray)
+        });
+    } else {
+        // GET SPECIFIED DOCS
+        for(let propertyID in propertyIDArray) {
+            const docRef = doc(db, "units", propertyIDArray[propertyID]);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()){
+                data[docSnap.id] = await getFields(docSnap, includeFieldsArray, omitFieldsArray)
+}
+            else  data[docSnap.id] = "propertyID not found: " + propertyIDArray[propertyID]
+        }
+        }
+    return data
+}
---
+++ public/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA3vX6rlSeg-mBJ--ZJt6Z7FsqeK_nPhj0",
+    authDomain: "luminous-lambda-364207.firebaseapp.com",
+    projectId: "luminous-lambda-364207",
+    storageBucket: "luminous-lambda-364207.appspot.com",
+    messagingSenderId: "518969290682",
+    appId: "1:518969290682:web:0ed4fde6c877da0fd4f783"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
